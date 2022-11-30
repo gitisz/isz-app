f697547ac2b01b01fa393bbda29fef675d4e2036
isz <git@web-wired.com>
2022-11-30 03:38:56 -0500

    No settings menu if no menu items

--- src/components/SingleColumn.tsx
+++ src/components/SingleColumn.tsx
@@ -130,70 +130,74 @@ export const SingleColumn: React.FC<LayoutProps> = ({ children }) => {
             </Typography>
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
             <Box sx={{ flexGrow: 0, marginLeft: 'auto' }}>
-              <Tooltip title="Settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  sx={{ ml: 2 }}
-                  aria-controls={m_open ? 'account-menu' : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={m_open ? 'true' : undefined}
-                >
-                  <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-                </IconButton>
-              </Tooltip>
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={m_open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: 'visible',
-                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                    mt: 1.5,
-                    '& .MuiAvatar-root': {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    '&:before': {
-                      content: '""',
-                      display: 'block',
-                      position: 'absolute',
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: 'background.paper',
-                      transform: 'translateY(-50%) rotate(45deg)',
-                      zIndex: 0,
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-              >
-                {Object.keys(menuItems).map((name) => {
-                  const item = menuItems[name];
-                  if (item.settings.type === 'header') {
-                    const Component = item.component;
-                    return (
-                      <List>
-                        {
-                          [menuItems].map((text, index) => (
-                            <Component />
-                          ))
-                        }
-                      </List>
-                    );
-                  }
-                  return undefined;
-                })}
-              </Menu>
+              {Object.keys(menuItems).filter(name => menuItems[name].settings.type === 'header').length > 0 ?
+                <>
+                  <Tooltip title="Settings">
+                    <IconButton
+                      onClick={handleClick}
+                      size="small"
+                      sx={{ ml: 2 }}
+                      aria-controls={m_open ? 'account-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={m_open ? 'true' : undefined}
+                    >
+                      <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="settings-menu"
+                    open={m_open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                      elevation: 0,
+                      sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                          width: 32,
+                          height: 32,
+                          ml: -0.5,
+                          mr: 1,
+                        },
+                        '&:before': {
+                          content: '""',
+                          display: 'block',
+                          position: 'absolute',
+                          top: 0,
+                          right: 14,
+                          width: 10,
+                          height: 10,
+                          bgcolor: 'background.paper',
+                          transform: 'translateY(-50%) rotate(45deg)',
+                          zIndex: 0,
+                        },
+                      },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                  >
+                    {Object.keys(menuItems).map((name) => {
+                      const item = menuItems[name];
+                      if (item.settings.type === 'header') {
+                        const Component = item.component;
+                        return (
+                          <List>
+                            {
+                              [menuItems].map((text, index) => (
+                                <Component />
+                              ))
+                            }
+                          </List>
+                        );
+                      }
+                      return undefined;
+                    })}
+                  </Menu>
+                </>
+                : null}
             </Box>
           </Toolbar>
         </AppBar>
